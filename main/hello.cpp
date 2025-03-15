ea6604a78ed2745cf96988868a4c010b2bcdb8a1
rohitm02 <rohit.maddaly@gmail.com>
2025-03-15 17:05:35 +0530

    madecppchange

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World. I'm PES2UG22CS459\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
