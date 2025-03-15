c5cd65714c82b58fbc1b8d5182afb13f6b0634c8
rohitm02 <rohit.maddaly@gmail.com>
2025-03-15 18:52:04 +0530

    Updated hello.cpp with a new message

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World. I'm PES2UG22CS459\n";
+  cout << "Hello, Jenkins! Now with a change.";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
